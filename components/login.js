f0fb3958eb950f529193fe8caa7e95b7eab27e2e
gaspyoddovero742 <gaspyoddovero742@gmail.com>
2024-06-27 13:55:23 +0000

    cambios css

---
+++ components/login.js
@@ -0,0 +1,8 @@
+function habilitarRegistrar(){
+    document.querySelector(".contenedor_Login").style.display = "none";
+    document.querySelector(".contenedor_Registrar").style.display = "flex";
+}
+function habilitarLogin(){
+    document.querySelector(".contenedor_Login").style.display = "flex";
+    document.querySelector(".contenedor_Registrar").style.display = "none";
+}
