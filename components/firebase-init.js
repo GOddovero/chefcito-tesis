26bf5cdb597354fa44515105d70062244cff6dc6
Gaspar Oddovero Herrera <100934173+GOddovero@users.noreply.github.com>
2024-08-20 16:43:38 -0300

    Update firebase-init.js

--- components/firebase-init.js
+++ components/firebase-init.js
@@ -1,11 +1,11 @@
 // firebase-init.js
 const firebaseConfig = {
-    apiKey: "AIzaSyD9AikH55edE0Vp7lBiau-q1XQgxLSZg2s",
-    authDomain: "chefcito-af900.firebaseapp.com",
-    projectId: "chefcito-af900",
-    storageBucket: "chefcito-af900.appspot.com",
-    messagingSenderId: "101121755447",
-    appId: "1:101121755447:web:407209242cf415d3b9b2b2"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
